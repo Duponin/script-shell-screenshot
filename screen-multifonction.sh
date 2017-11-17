766c0d24c15fee503a2011a46e305670504e42f2
Duponin <duponin@gmail.com>
2017-11-17 10:18:23 +0100

    add simple log fonctionnality

--- screen-multifonction.sh
+++ screen-multifonction.sh
@@ -10,7 +10,10 @@ hauteur=600
 cd $workingDir
 mkdir $date
 cd $date
-scrot -s $date.full.jpg
-convert $date.full.jpg -resize 10% $date.jpg
+touch output.log
+scrot -s $date.full.jpg && echo "screenshot  successfull" >> output.log
+convert $date.full.jpg -resize 10% $date.jpg && echo "10% resize  successfull" >> output.log
 convert $date.full.jpg -resize $largeur\x$hauteur -background black -compose Copy \
-       -gravity center -extent $largeur\x$hauteur -quality 92 $date.calibrated.jpg
+       -gravity center -extent $largeur\x$hauteur -quality 92 $date.calibrated.jpg \
+       && echo "calibration successfull" >> output.log
+echo "script end" >> output.log
